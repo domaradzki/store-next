598ad9ca5d8d0114e33d86d0bcfb06059366e9e8
Grzegorz Domaradzki <domaradzki.g@gmail.com>
2021-09-18 15:35:08 +0200

    added User schema and authorization

---
+++ backend/keystone.ts
@@ -0,0 +1,57 @@
+import { createAuth } from '@keystone-next/auth';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+
+import 'dotenv/config';
+import { User } from './schemas/User';
+
+const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360, // How long they stay signed in
+  secret: process.env.COOKIE_SECRET,
+};
+
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO add in initial roles
+  },
+});
+
+export default withAuth(
+  config({
+    // @ts-ignore
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+      // TODO: add data seeding here
+    },
+    lists: createSchema({
+      // TODO: add data seeding here
+      User,
+    }),
+    ui: {
+      // Show this UI only for peaple who pass this test
+      isAccessAllowed: ({ session }) => {
+        console.log(session);
+        return !!session?.data;
+      },
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: 'id',
+    }),
+  })
+);
