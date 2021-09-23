2a5cb36d996b8e35ee7115b1607d78ef6a2e1117
Grzegorz Domaradzki <domaradzki.g@gmail.com>
2021-09-23 15:01:32 +0200

    Error message displayed, mutation to adding products added with clering form and redirecting to the product page

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -1,22 +1,60 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
+import Router from 'next/router';
 import useForm from '../lib/useForm';
+import ErrorMessage from './ErrorMessage';
+import { ALL_PRODUCTS_QUERY } from './Products';
 import Form from './styles/Form';
+
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ) {
+    createProduct(
+      data: {
+        name: $name
+        description: $description
+        price: $price
+        status: "AVAILABLE"
+        photo: { create: { image: $image, altText: $name } }
+      }
+    ) {
+      id
+      price
+      description
+      name
+    }
+  }
+`;
 
 function CreateProduct() {
   const { inputs, handleChange, resetForm, clearForm } = useForm({
-    name: 'Kosmetyk',
+    name: '',
     description: '',
     price: 0,
     image: '',
   });
+  const [createProduct, { loading, error, data }] = useMutation(
+    CREATE_PRODUCT_MUTATION,
+    { variables: inputs, refetchQueries: [{ query: ALL_PRODUCTS_QUERY }] }
+  );
 
   return (
     <Form
-      onSubmit={(e) => {
+      onSubmit={async (e) => {
         e.preventDefault();
-        console.log(inputs);
+        const res = await createProduct();
+        clearForm();
+        Router.push({
+          pathname: `/product/${res.data.createProduct.id}`,
+        });
       }}
     >
-      <fieldset>
+      <ErrorMessage error={error} />
+      <fieldset disabled={loading} aria-busy={loading}>
         <label htmlFor="image">
           Image:
           <input type="file" id="image" name="image" onChange={handleChange} />
