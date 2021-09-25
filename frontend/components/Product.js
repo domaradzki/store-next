d4e6d9f5903b80b946602a9c5c3236099dc1e0f4
Grzegorz Domaradzki <domaradzki.g@gmail.com>
2021-09-25 18:00:34 +0200

    added delete button and functionality to delete products with confirmation

--- frontend/components/Product.js
+++ frontend/components/Product.js
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import formatMoney from '../lib/formatMoney';
+import DeleteProduct from './DeleteProduct';
 
 import ItemStyles from './styles/ItemStyles';
 import PriceTag from './styles/PriceTag';
@@ -21,6 +22,7 @@ export default function Product({ product }) {
         <Link href={{ pathname: 'update', query: { id: product.id } }}>
           Edit 🖉
         </Link>
+        <DeleteProduct id={product.id}>Delete</DeleteProduct>
       </div>
     </ItemStyles>
   );
