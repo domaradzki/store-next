6752f745cbe463fac46e2f56433a6206d44b6461
Grzegorz Domaradzki <domaradzki.g@gmail.com>
2021-09-26 11:35:04 +0200

    added component Pagination and render the actual links

---
+++ frontend/components/Pagination.js
@@ -0,0 +1,42 @@
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+import Head from 'next/head';
+import Link from 'next/link';
+
+import { perPage } from '../config';
+import ErrorMessage from './ErrorMessage';
+import PaginationStyles from './styles/PaginationStyles';
+
+const PAGINATION_QUERY = gql`
+  query {
+    _allProductsMeta {
+      count
+    }
+  }
+`;
+
+export default function Pagination({ page }) {
+  const { data, loading, error } = useQuery(PAGINATION_QUERY);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <ErrorMessage error={error} />;
+  const { count } = data._allProductsMeta;
+  const pageCount = Math.ceil(count / perPage);
+  return (
+    <PaginationStyles>
+      <Head>
+        <title>MarocanOil - Page {page} of ___</title>
+      </Head>
+      <Link href={`/products/${page - 1}`}>
+        <a aria-disabled={page <= 1}>← Prev</a>
+      </Link>
+      <p>
+        Page {page} of {pageCount}
+      </p>
+      <p>{count} Items Total</p>
+
+      <Link href={`/products/${page + 1}`}>
+        <a aria-disabled={page >= pageCount}>Next →</a>
+      </Link>
+    </PaginationStyles>
+  );
+}
